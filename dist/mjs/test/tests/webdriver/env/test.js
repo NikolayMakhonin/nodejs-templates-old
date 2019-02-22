d339d7a18588579742a26203de503b95745bd2e7
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-02-22 12:07:04 +0200

    Fix coverage

---
+++ dist/mjs/test/tests/webdriver/env/test.js
@@ -0,0 +1,28 @@
+var _intern$getInterface = intern.getInterface('object'),
+    registerSuite = _intern$getInterface.registerSuite;
+
+var _intern$getPlugin = intern.getPlugin('chai'),
+    assert = _intern$getPlugin.assert;
+
+console.log('before registerSuite');
+
+var keys = require('@theintern/leadfoot/keys');
+
+registerSuite('Todo (functional)', {
+  'submit form': function submitForm() {
+    console.log('submit form');
+    var result = this.remote // .get('/index.html2')
+    .get(pathToUrl(__dirname, 'assets/index.html')) // .findByCssSelector('.new-toodo')
+    // .type('Task 1')
+    // .type(keys.RETURN)
+    // .type('Task 2')
+    // .type(keys.RETURN)
+    // .type('Task 3')
+    // .getSpecAttribute('value')
+    .then(function (value) {
+      console.log(value);
+    });
+    return result;
+  }
+});
+console.log('after registerSuite');
