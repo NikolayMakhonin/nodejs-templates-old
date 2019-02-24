a010986df6b931798225fd329c0885b42463b9ce
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-02-24 15:14:27 +0200

    develop

---
+++ env/deploy/deploy-config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+module.exports = {
+	username: 'balda',
+	password: 'nibelungia2',
+	url     : 'https://xmika.com/balda/upload.php',
+	dir     : path.resolve(__dirname, '../__sapper__/export/balda/page')
+}
